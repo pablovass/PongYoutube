b21960ac51d664c36ae5505a351cedfb7e4f3c2f
Pablo Vallejos <faktory.fm@live.com>
2021-11-25 08:04:50 -0300

    BallBehaviour

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -1,17 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 [RequireComponent(typeof(CircleCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class BallBehaviour : MonoBehaviour
 {
+    //[SerializeField]
+    public Rigidbody2D _rigidbody2D;
 
-    public Rigidbody2D _rigidbody2d;
     public Transform player;
-    //el bool siempre inicia con falso
-    bool gameStarted = false;
-  // float buttonMouse = Input.GetAxis("Jump");
+    bool gameStated = false;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,43 +20,24 @@ public class BallBehaviour : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        if (!gameStated)
+        {
+            float positionDiferent = player.position.x - transform.position.x;
 
-        //transform.position = player.position;
-        //float positionDiferent =  player.position.x- transform.position.x;
-        //transform.position = new Vector3(player.position.x - positionDiferent, player.position.y, player.position.z);
+            transform.position = new Vector3(player.position.x - positionDiferent, player.position.y, player.position.z);
 
+            if (Input.GetMouseButtonDown(0))
+            {
+                _rigidbody2D.velocity = new Vector2(8, 8);
+                gameStated = true;
 
-        //entoces lo que hacemos es 
+            }
+        }
         
-
-
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (!gameStarted)
-        {
-            float positionDiferent = player.position.x - transform.position.x;
-            transform.position = new Vector3(player.position.x - positionDiferent, player.position.y, player.position.z);
-            
-            if (Input.GetMouseButtonDown(0))
-            {
-                _rigidbody2d.velocity = new Vector2(8, 8);
-                gameStarted = true;
-            }
-        }
+        
     }
 }
-/*
- tenemos que cambiar la escala de la gravedad global de todo el juego 
-
-
-MATERIA DE FISICA EN EL COLLIDER 
-mATERIAL (CARPETA) 
-VAMOS A CREAR UN PHISICS MATERIAL 2D (Bouncy) 
-FRICTION DE LA DEJAMOS A 0 
-EL BOUNCINESS A 1 
-
-una vez modificado tenemos que agregarselo a collider
- */
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         float vertical = Input.GetAxis("Mouse Y");
+
         Vector3 mousePositionScreen = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //position x y z  
         transform.position = new Vector3(transform.position.x,Mathf.Clamp(mousePositionScreen.y,-2f,2f),transform.position.z);
